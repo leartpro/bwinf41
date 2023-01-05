80d5e9a3532806e1d7ea7ce637f5c52e5581cba6
leartpro <lenni.protte@gmx.de>
2023-01-05 13:23:20 +0100

    Output V3

--- LennartProtte/Aufgabe1-Implementierung/main.cpp
+++ LennartProtte/Aufgabe1-Implementierung/main.cpp
@@ -8,33 +8,39 @@
 
 using namespace std;
 
-// Struktur, um Koordinaten zu speichern
-struct Coordinate {
-    double x;
-    double y;
-};
-
-// Funktionsprototyp
-void process_files(const std::string &input_dir, const std::string &output_dir);
-
-double calculate_angle(Coordinate c1, Coordinate c2) {
-    double x_diff = c2.x - c1.x;
-    double y_diff = c2.y - c1.y;
-    return atan2(y_diff, x_diff) * 180 / M_PI;
+double calculate_angle(pair<double, double> node1, pair<double, double> node2) {
+    double x1 = node1.first;
+    double y1 = node1.second;
+    double x2 = node2.first;
+    double y2 = node2.second;
+    double angle = atan2(y2 - y1, x2 - x1);
+    angle = angle * 180 / M_PI;
+    return angle;
 }
 
-vector<vector<double>> create_weighted_graph(vector<Coordinate> coordinate_list) {
-    int num_nodes = coordinate_list.size();
-    vector<vector<double>> graph(num_nodes, vector<double>(num_nodes, 0));
-    for (int i = 0; i < num_nodes; i++) {
-        for (int j = i + 1; j < num_nodes; j++) {
-            double edge_weight = calculate_angle(coordinate_list[i], coordinate_list[j]);
-            graph[i][j] = edge_weight;
-            graph[j][i] = edge_weight;
+vector<pair<pair<double, double>, vector<pair<int, double>>>> read_and_store_coordinates(
+        std::vector<std::pair<double, double>>& coordinate_list) {
+    vector<pair<pair<double, double>, vector<pair<int, double>>>> graph;
+    // Iteriere über jede Koordinate in der Liste
+    for (const auto & i : coordinate_list) {
+        // Splitte die Koordinaten in x und y
+        // Füge die Koordinate als Knoten zum Graphen hinzu
+        graph.push_back({{i.first, i.second}, {}});
+    }
+    // Iteriere über jeden Knoten im Graphen
+    for (int i = 0; i < graph.size(); i++) {
+        // Iteriere über jeden anderen Knoten im Graphen
+        for (int j = 0; j < graph.size(); j++) {
+            if (i == j) continue;
+            // Berechne den Winkel zwischen dem aktuellen Knoten und dem anderen Knoten
+            double angle = calculate_angle(graph[i].first, graph[j].first);
+            // Füge eine Kante von dem aktuellen Knoten zu dem anderen Knoten hinzu
+            graph[i].second.emplace_back(j, angle);
         }
     }
     return graph;
 }
+
 
 void process_files(const std::string &input_dir, const std::string &output_dir) {
     // Iterator erstellen, der alle Dateien im Eingabeordner durchläuft
@@ -47,22 +53,20 @@ void process_files(const std::string &input_dir, const std::string &output_dir) 
         // Ausgabedatei öffnen
         std::ofstream fout(output_file);
         // Vector, um die Koordinaten zu speichern
-        vector<Coordinate> coordinate_list;
+        std::vector<std::pair<double, double>> coordinate_list;
         // Solange es noch Koordinaten in der Datei gibt
-        while (fin.good()) {
-            Coordinate coord{};
-            // Lese die x- und y-Koordinate aus der Datei ein
-            fin >> coord.x >> coord.y;
-            // Füge die Koordinate zur Liste hinzu
-            coordinate_list.push_back(coord);
+        double x, y;
+        while (fin >> x >> y) {
+            coordinate_list.emplace_back(x, y);
         }
-        vector<vector<double>> graph = create_weighted_graph(coordinate_list);
+        auto graph = read_and_store_coordinates(coordinate_list);
         // Graph in Ausgabedatei schreiben
-        for (auto & row : graph) {
-            for (double val : row) {
-                fout << val << ' ';
+        for (int i = 0; i < graph.size(); i++) {
+            fout << "Knoten " << i << ": (" << graph[i].first.first << ", " << graph[i].first.second << ")" << endl;
+            fout << "Kanten:" << endl;
+            for (auto & j : graph[i].second) {
+                fout << "  - Knoten " << j.first << ": Winkel " << j.second << endl;
             }
-            fout << endl;
         }
         // Dateien schließen
         fin.close();
