fdb2a845a5b6baca3dce1232c509f9affe45b2e8
leartpro <lenni.protte@gmx.de>
2023-01-18 22:20:00 +0100

    V33 debug is working

--- LennartProtte/Aufgabe2-Implementierung/main.cpp
+++ LennartProtte/Aufgabe2-Implementierung/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <filesystem>
+#include <cmath>
+#include <fstream>
 
 using namespace std;
 
@@ -9,45 +12,103 @@ struct Slice {
         this->length = a;
         this->height = b;
     }
-
     int length, height;
 };
 
-bool calculate_cube(int length, int height, int depth, vector<pair<Slice, int>> &order, vector<Slice> &slices, int counter);
+bool calculate_cube(int length, int height, int depth, vector<pair<Slice, int>> &order, vector<Slice> &slices);
+
 
 int main() {
-    int length = 6, height = 6, depth = 6;
-    vector<pair<Slice, int>> order;
+    string input_dir = "../LennartProtte/Aufgabe2-Implementierung/Eingabedateien";
+    string output_dir = "../LennartProtte/Aufgabe2-Implementierung/Ausgabedateien";
 
-    vector<Slice> slices = {
-            {2, 4},
-            {4, 6},
-            {6, 6},
-            {3, 4},
-            {4, 6},
-            {3, 6},
-            {2, 4},
-            {2, 4},
-            {4, 6},
-            {3, 3},
-            {3, 3},
-            {6, 6}
-    };
-    /*
-    sort(slices.begin(), slices.end(), [](Slice a, Slice b) {
-             return (a.length * a.height) > (b.length * b.height);
-         }
-    );
-    */
-    if (calculate_cube(length, height, depth, order, slices, 1)) {
-        cout << "Die Käsescheiben können zu einem Quader zusammengesetzt werden:" << endl;
-        for (auto const &o: order) {
-            cout << "Slice: (" << o.first.length << ", " << o.first.height << ") Dimension: " << o.second << endl;
+    //string input_dir = "./TestInput";
+    //string output_dir = "./TestOutput";
+
+    // Iterator erstellen, der alle Dateien im Eingabeordner durchläuft
+    for (const auto &entry: filesystem::directory_iterator(input_dir)) {
+
+        // Dateiname und -pfad aus dem Iterator auslesen
+        string input_file = entry.path();
+        string output_file = output_dir + "/" + entry.path().filename().string();
+
+        // Eingabedatei öffnen
+        ifstream fin(input_file);
+
+        // Ausgabedatei öffnen
+        ofstream fout(output_file);
+
+        //Ausgabedatei einlesen
+        vector<Slice> slices;
+        int a, b, n;
+        fin >> n;
+        while (fin >> a >> b) {
+            slices.emplace_back(a, b);
         }
-    } else {
-        cout << "Die Käsescheiben können nicht zu einem Quader zusammengesetzt werden." << endl;
-    }
 
+        // Calculate the volume of the cheese cube
+        int volume = 0;
+        for (const auto &slice: slices) {
+            volume += slice.length * slice.height;
+        }
+
+        // Calculate the dimensions of the cheese cube
+        int length = 0, height = 0, depth = 0, buffer = 0;
+        for(const auto &slice: slices) {
+            if (slice.length > length || slice.height > length || slice.length > height ||slice.height > height) {
+                length = slice.length;
+                height = slice.height;
+            }
+        }
+        depth = volume / (length * height);
+
+        // Calculate the Buffer of the cheese cube
+        if(length * height * depth != volume) {
+            if (length * height * depth > volume) {
+                cout << "Die Scheiben können nicht zu einem Quader zusammengesetzt werden." << endl;
+                fout << "Die Scheiben können nicht zu einem Quader zusammengesetzt werden." << endl;
+                fin.close();
+                fout.close();
+                continue;
+            } else {
+                while (length * height * (depth + buffer) < volume) {
+                    buffer++;
+                }
+                if(length * height * depth != volume) {
+                    cout << "Die Scheiben können nicht zu einem Quader zusammengesetzt werden." << endl;
+                    fout << "Die Scheiben können nicht zu einem Quader zusammengesetzt werden." << endl;
+                    fin.close();
+                    fout.close();
+                    continue;
+                }
+            }
+        }
+
+        cout << "Quader " << length << "x" << height << "x" << depth << " + buffer=" << buffer << endl << endl;
+        fout << "Quader " << length << "x" << height << "x" << depth << " + buffer=" << buffer << endl << endl;
+        //get result
+        vector<pair<Slice, int>> order;
+        sort(slices.begin(), slices.end(), [](Slice a, Slice b) {
+                 return (a.length * a.height) > (b.length * b.height);
+             }
+        );
+
+        if (calculate_cube(length, height, depth, order, slices)) {
+            cout << "Die Scheiben können zu einem Quader zusammengesetzt werden:" << endl;
+            fout << "Die Scheiben können zu einem Quader zusammengesetzt werden:" << endl;
+            for (auto const &o: order) {
+                cout << "Slice: (" << o.first.length << ", " << o.first.height << ") Dimension: " << o.second << endl;
+                fout << "Slice: (" << o.first.length << ", " << o.first.height << ") Dimension: " << o.second << endl;
+            }
+        } else {
+            cout << "Die Scheiben können nicht zu einem Quader zusammengesetzt werden." << endl;
+            fout << "Die Scheiben können nicht zu einem Quader zusammengesetzt werden." << endl;
+        }
+
+        // Dateien schließen
+        fin.close();
+        fout.close();
+    }
     return 0;
 }
 
@@ -62,22 +123,12 @@ int canRemoveSlice(int length, int height, int depth, Slice slice) {
         return -1; //DOES NOT FIT
     }
 }
-bool calculate_cube(int length, int height, int depth, vector<pair<Slice, int>> &order, vector<Slice> &slices, int counter) {
-    cout << "recursion: " << counter << endl;
-    cout << "cube: " << length << "x" << height << "x" << depth << "x" << endl;
-    cout << "ORDER: " << endl;
-    for (auto const &o: order) {
-        cout << "slice: (" << o.first.length << ", " << o.first.height << ") dimension: " << o.second << endl;
-    }
-    cout << "UNORDER: " << endl;
-    for (auto const &o: slices) {
-        cout << "slice: (" << o.length << ", " << o.height << ")" << endl;
-    }
+
+bool calculate_cube(int length, int height, int depth, vector<pair<Slice, int>> &order, vector<Slice> &slices) {
     if (slices.empty()) {
-        cout << "CUBE COMPLETED" << endl;
-        cout << "RETURN recursion " << counter << endl;
         return (length == 0 || height == 0 || depth == 0);
     }
+    vector<Slice> removed_slices;
     for (auto it = slices.begin(); it != slices.end(); ++it) {
         int dimension = canRemoveSlice(length, height, depth, *it);
         if (dimension >= 0) {
@@ -85,24 +136,20 @@ bool calculate_cube(int length, int height, int depth, vector<pair<Slice, int>> 
             int new_height = height - (dimension == 1 ? 1 : 0);
             int new_depth = depth - (dimension == 0 ? 1 : 0);
             order.emplace_back(*it, dimension);
+            removed_slices.push_back(*it);
             slices.erase(it);
-            cout << endl;
-            if (calculate_cube(new_length, new_height, new_depth, order, slices, counter+1)) {
-                cout << "CUBE COMPLETED" << endl;
-                cout << "RETURN recursion " << counter << endl;
+            if (calculate_cube(new_length, new_height, new_depth, order, slices)) {
                 return true;
             } else {
                 order.pop_back();
-                slices.push_back(*it);
+                for (auto& slice : removed_slices) {
+                    slices.push_back(slice);
+                }
+                removed_slices.clear();
             }
         }
     }
-    cout << "ITERATION FAILED => RETURN FALSE" << endl;
-    cout << "RETURN recursion " << counter << endl;
     return false;
 }
-
-
-
 
 
