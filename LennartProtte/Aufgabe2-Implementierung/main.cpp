1278dd599ce2be322c9181592773749cad51b2aa
leartpro <lenni.protte@gmx.de>
2023-01-18 17:19:19 +0100

    V33 debug print

--- LennartProtte/Aufgabe2-Implementierung/main.cpp
+++ LennartProtte/Aufgabe2-Implementierung/main.cpp
@@ -13,7 +13,7 @@ struct Slice {
     int length, height;
 };
 
-bool calculate_cube(int length, int height, int depth, vector<pair<Slice, int>> &order, vector<Slice> &slices);
+bool calculate_cube(int length, int height, int depth, vector<pair<Slice, int>> &order, vector<Slice> &slices, int counter);
 
 int main() {
     int length = 6, height = 6, depth = 6;
@@ -39,7 +39,7 @@ int main() {
          }
     );
     */
-    if (calculate_cube(length, height, depth, order, slices)) {
+    if (calculate_cube(length, height, depth, order, slices, 1)) {
         cout << "Die Käsescheiben können zu einem Quader zusammengesetzt werden:" << endl;
         for (auto const &o: order) {
             cout << "Slice: (" << o.first.length << ", " << o.first.height << ") Dimension: " << o.second << endl;
@@ -62,29 +62,34 @@ int canRemoveSlice(int length, int height, int depth, Slice slice) {
         return -1; //DOES NOT FIT
     }
 }
-
-bool calculate_cube(int length, int height, int depth, vector<pair<Slice, int>> &order, vector<Slice> &slices) {
+bool calculate_cube(int length, int height, int depth, vector<pair<Slice, int>> &order, vector<Slice> &slices, int counter) {
+    cout << "recursion: " << counter << endl;
     cout << "cube: " << length << "x" << height << "x" << depth << "x" << endl;
+    cout << "ORDER: " << endl;
+    for (auto const &o: order) {
+        cout << "slice: (" << o.first.length << ", " << o.first.height << ") dimension: " << o.second << endl;
+    }
+    cout << "UNORDER: " << endl;
+    for (auto const &o: slices) {
+        cout << "slice: (" << o.length << ", " << o.height << ")" << endl;
+    }
     if (slices.empty()) {
         cout << "CUBE COMPLETED" << endl;
+        cout << "RETURN recursion " << counter << endl;
         return (length == 0 || height == 0 || depth == 0);
     }
     for (auto it = slices.begin(); it != slices.end(); ++it) {
         int dimension = canRemoveSlice(length, height, depth, *it);
         if (dimension >= 0) {
-            cout << "current slice: {" << it->length << ", " << it->height << "}" << endl;
-            cout << "current dimension for slice: " << dimension << endl;
             int new_length = length - (dimension == 2 ? 1 : 0);
             int new_height = height - (dimension == 1 ? 1 : 0);
             int new_depth = depth - (dimension == 0 ? 1 : 0);
             order.emplace_back(*it, dimension);
             slices.erase(it);
-            cout << "ORDER: " << endl;
-            for (auto const &o: order) {
-                cout << "slice: (" << o.first.length << ", " << o.first.height << ") dimension: " << o.second << endl;
-            }
             cout << endl;
-            if (calculate_cube(new_length, new_height, new_depth, order, slices)) {
+            if (calculate_cube(new_length, new_height, new_depth, order, slices, counter+1)) {
+                cout << "CUBE COMPLETED" << endl;
+                cout << "RETURN recursion " << counter << endl;
                 return true;
             } else {
                 order.pop_back();
@@ -93,6 +98,7 @@ bool calculate_cube(int length, int height, int depth, vector<pair<Slice, int>> 
         }
     }
     cout << "ITERATION FAILED => RETURN FALSE" << endl;
+    cout << "RETURN recursion " << counter << endl;
     return false;
 }
 
