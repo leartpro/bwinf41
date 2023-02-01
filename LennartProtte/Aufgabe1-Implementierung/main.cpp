ec3fe2e5d47825c89caeb5241b8880c2eb18d28c
leartpro <lenni.protte@gmx.de>
2023-02-01 12:33:28 +0100

    V48 SAT Solver Approach re-structuring

--- LennartProtte/Aufgabe1-Implementierung/main.cpp
+++ LennartProtte/Aufgabe1-Implementierung/main.cpp
@@ -1,20 +1,10 @@
 #include <vector>
 #include <iostream>
+#include <unordered_map>
 #include <cmath>
 
-// Umrechnung der 2D-Punktkoordinate in 1D-Variable
-int getVariable(int i, int j, int n) {
-    return (i - 1) * n + j - 1;
-}
-
-// Umrechnung der 1D-Variable in 2D-Punktkoordinate
-std::pair<int, int> getCoordinateIndexes(int v, int n) {
-    int i = v / n + 1;
-    int j = v % n + 1;
-    return std::make_pair(i, j);
-}
-
 //Checks if a vertex is reachable from another vertex
+/*
 bool is_reachable(const int &current,
                   std::vector<int> &route,
                   const int &last_index,
@@ -34,6 +24,7 @@ bool is_reachable(const int &current,
     }
     return false;
 }
+ */
 
 //Checks if current is not excluded by the clauses set by route
 bool is_not_excluded(const int &current,
@@ -59,6 +50,7 @@ bool is_not_excluded(const int &current,
     return true;
 }
 
+/*
 bool route_satisfied(std::vector<int> &route,
                      const int &count_of_nodes,
                      const std::vector<std::pair<int, int>> &not_together_clauses,
@@ -75,7 +67,9 @@ bool route_satisfied(std::vector<int> &route,
         return false;
     }
 }
+ */
 
+/*
 bool sat_solver(std::vector<int> &vertexes,
                 std::vector<int> &route,
                 const std::vector<std::pair<int, int>> &not_together_clauses,
@@ -106,6 +100,7 @@ bool sat_solver(std::vector<int> &vertexes,
     std::cout << "recursion FAILED " << std::endl;
     return false;
 }
+ */
 
 int main() {
     // Beispieldatensatz von 2d-Punktkoordinaten
@@ -127,84 +122,76 @@ int main() {
     int n = int(coordinates.size());
     std::vector<std::pair<int, int>> not_together_clauses; //they cant be after each other
     std::vector<std::vector<int>> one_existing_clauses; //only one of them can exist in solution
-    std::vector<int> vertexes;
-    vertexes.reserve(
-            (n * (n - 1))); //maximum count of vertexes (because every vertex is stored for every direction)
+    std::unordered_map<int, std::pair<std::pair<double, double>, std::pair<double, double>>> graph;
+    graph.reserve(n * (n - 1)); //maximum count of vertexes (because every vertex is stored for every direction)
 
     //1. clause that every edge can only used once
     //2. creates vector with all possible edges
+    int key = 0;
     for (int i = 0; i < n; i++) {
         for (int j = i + 1; j < n; j++) {
             if (i != j) {
-                vertexes.push_back(getVariable(i, j, n));
-                vertexes.push_back(getVariable(j, i, n));
-                one_existing_clauses.push_back({getVariable(i, j, n), getVariable(j, i, n)});
+                auto value_1 = std::make_pair(coordinates[i], coordinates[j]);
+                auto value_2 = std::make_pair(coordinates[j], coordinates[i]);
+                graph.insert(std::make_pair(key, value_1));
+                key++;
+                graph.insert(std::make_pair(key, value_2));
+                key++;
+                one_existing_clauses.push_back({key - 1, key});
             }
         }
     }
 
     //creates not_together_clauses that contains if two edges can not be behind each other
-    for (int i = 0; i < n; i++) {
-        for (int j = i + 1; j < n; j++) {
-            for (int k = j + 1; k < n; k++) {
-                if (i != j && i != k && j != k) { //find all tripple
-                    for (int l = 0; l < 3; l++) { //for ech possible order of the current tripple
-                        std::pair<double, double> v1;
-                        v1.first = coordinates[j].first - coordinates[i].first;
-                        v1.second = coordinates[j].second - coordinates[i].second;
-                        std::pair<double, double> v2;
-                        v2.first = coordinates[j].first - coordinates[k].first;
-                        v2.second = coordinates[j].second - coordinates[k].second;
-                        double dot_product = v1.first * v2.first + v1.second * v2.second;
-                        double v1_length = sqrt(v1.first * v1.first + v1.second * v1.second);
-                        double v2_length = sqrt(v2.first * v2.first + v2.second * v2.second);
-                        double angle = acos(dot_product / (v1_length * v2_length));
-                        angle = angle * 180 / M_PI; // Umrechnung in Grad des innen Winkels alpha
-                        if (angle < 0) {
-                            angle += 360; // Winkel im Bereich von 0 bis 360
-                        }
-                        if (angle < 90) {
-                            not_together_clauses.emplace_back(getVariable(i, j, n), getVariable(j, k, n));
-                        }
-                        int t = i;
-                        i = j;
-                        j = k;
-                        k = t;
+    for (auto i = graph.begin(); i != graph.end(); i++) {
+        for (auto j = graph.begin(); j != graph.end(); j++) {
+            if(i != j) {
+                if(i->second.second == j->second.first) {
+                    std::pair<double, double> v1;
+                    v1.first = i->second.second.first - i->second.first.first;
+                    v1.second = i->second.second.second - i->second.first.second;
+                    std::pair<double, double> v2;
+                    v2.first = j->second.first.first - j->second.second.first;
+                    v2.second = j->second.first.second - j->second.second.second;
+                    double dot_product = v1.first * v2.first + v1.second * v2.second;
+                    double v1_length = sqrt(v1.first * v1.first + v1.second * v1.second);
+                    double v2_length = sqrt(v2.first * v2.first + v2.second * v2.second);
+                    double angle = acos(dot_product / (v1_length * v2_length));
+                    angle = angle * 180 / M_PI; // Umrechnung in Grad des innen Winkels alpha
+                    std::cout << "(" << v1.first << "," << v1.second << ") -> (" << v2.first << "," << v2.second << ") => " << angle << "°" << std::endl;
+                    if (angle < 0) {
+                        angle += 360; // Winkel im Bereich von 0 bis 360
                     }
-                    //rotate to default
-                    int t = i;
-                    i = j;
-                    j = k;
-                    k = t;
+                    if (angle < 90) {
+                        not_together_clauses.emplace_back(i->first, j->first);
+                    }
                 }
             }
         }
     }
 
     //create clauses entry for every node, it is only allowed to use two of all edges connected to him
-    for (int i = 0; i < n; i++) {
+    for (auto coordinate : coordinates) {
         std::vector<int> from_node, to_node;
-        from_node.reserve(vertexes.size() - 1);
-        to_node.reserve(vertexes.size() - 1);
-        for (const auto &vertex: vertexes) {
-            if (getCoordinateIndexes(vertex, n).first == i) {
-                from_node.push_back(vertex);
-            } else if (getCoordinateIndexes(vertex, n).second == i) {
-                to_node.push_back(vertex);
+        for (auto &i: graph) {
+            if (i.second.first == coordinate) {
+                from_node.push_back(i.first);
+            } else if (i.second.second == coordinate) {
+                to_node.push_back(i.first);
             }
+            one_existing_clauses.push_back(from_node);
+            one_existing_clauses.push_back(to_node);
         }
-        one_existing_clauses.push_back(from_node);
-        one_existing_clauses.push_back(to_node);
     }
 
     std::cout << "Vertexes: " << std::endl;
-    for (const auto &v: vertexes) {
-        std::cout << v << " | ";
+    for (auto & i : graph) {
+        std::cout << i.first << " | ";
         std::cout
-                << "FIRST:(" << coordinates[getCoordinateIndexes(v, n).first].first
-                << ", " << coordinates[getCoordinateIndexes(v, n).first].second
-                << ") -> SECOND:(" << coordinates[getCoordinateIndexes(v, n).second].first
-                << ", " << coordinates[getCoordinateIndexes(v, n).second].second
+                << "FIRST:(" << i.second.first.first
+                << ", " << i.second.first.second
+                << ") -> SECOND:(" << i.second.second.first
+                << ", " << i.second.second.second
                 << ")" << std::endl;
     }
     std::cout << std::endl;
@@ -222,7 +209,7 @@ int main() {
         }
         std::cout << std::endl;
     }
-
+/*
     std::vector<int> result;
     if (sat_solver(vertexes, result, not_together_clauses, one_existing_clauses)) {
         std::cout << "solution" << std::endl;
@@ -237,5 +224,6 @@ int main() {
     } else {
         std::cout << "no solution" << std::endl;
     }
+    */
     return 0;
 }
