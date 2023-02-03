a413b9b1d7e097137eddff2ed606dcd1cd6ae5ac
leartpro <lenni.protte@gmx.de>
2023-02-03 16:25:10 +0100

    V49 SAT Solver Approach completing tsp to sat reduction

--- LennartProtte/Aufgabe1-Implementierung/main.cpp
+++ LennartProtte/Aufgabe1-Implementierung/main.cpp
@@ -4,22 +4,21 @@
 #include <cmath>
 
 //Checks if a vertex is reachable from another vertex
-bool is_reachable(const int &current,
+bool is_reachable(const int &to,
                   std::vector<int> &route,
-                  const int &last,
-                  const std::unordered_map<int, std::pair<std::pair<double, double>, std::pair<double, double>>>& graph
-                  ) {
+                  const int &from,
+                  const std::unordered_map<int, std::pair<std::pair<double, double>, std::pair<double, double>>> &graph
+) {
     if (route.empty()) {
         return true;
     }
-    if (graph.find(current)->second.first == graph.find(last)->second.second) {
+    if (graph.find(from)->second.second == graph.find(to)->second.first) {
         return true;
     }
     return false;
 }
 
 //Checks if current is not excluded by the clauses set by route
-//TODO: THIS METHOD CAUsES BIG PROBLEMS
 bool is_not_excluded(const int &current,
                      std::vector<int> &route,
                      const std::vector<std::pair<int, int>> &not_together_clauses,
@@ -32,11 +31,13 @@ bool is_not_excluded(const int &current,
             return false;
         }
     }
-    for (int &vertex: route) { //TODO: wrong code
-        for (const auto &clause: one_existing_clauses) {
-            if (std::find(clause.begin(), clause.end(), vertex) != clause.end() &&
-                std::find(clause.begin(), clause.end(), current) != clause.end()) {
-                return false;
+    for (int &vertex: route) {
+        if (vertex != current) {
+            for (const auto &clause: one_existing_clauses) {
+                if (std::find(clause.begin(), clause.end(), vertex) != clause.end() &&
+                    std::find(clause.begin(), clause.end(), current) != clause.end()) {
+                    return false;
+                }
             }
         }
     }
@@ -44,20 +45,25 @@ bool is_not_excluded(const int &current,
 }
 
 bool route_satisfied(std::vector<int> &route,
-                     const std::unordered_map<int, std::pair<std::pair<double, double>, std::pair<double, double>>>& graph,
+                     const std::unordered_map<int, std::pair<std::pair<double, double>, std::pair<double, double>>> &graph,
                      const std::vector<std::pair<int, int>> &not_together_clauses,
-                     const std::vector<std::vector<int>> &one_existing_clauses) {
-    if(route.empty()) {
+                     const std::vector<std::vector<int>> &one_existing_clauses,
+                     const int& count_of_nodes) {
+    if (route.empty()) {
         return false;
     }
-    for (auto it = route.begin(); it != route.end(); ++it) {
-        bool is_not_reachable = !is_reachable(route[int(std::distance(route.begin(), it + 1))], route, *it, graph);
-        bool is_excluded = !is_not_excluded(*it, route, not_together_clauses, one_existing_clauses);
-        if (is_not_reachable || is_excluded) { //TODO: both conditions are always false
+    for(int i = 0; i < route.size() - 1; i++) {
+        bool is_not_reachable = !is_reachable(route[i + 1], route, route[i], graph);
+        bool is_excluded = !is_not_excluded(route[i], route, not_together_clauses, one_existing_clauses);
+        if (is_not_reachable || is_excluded) {
             return false;
         }
     }
-    if (route.size() == graph.size() / 2) { //TODO: total count of nodes graph.size() is 12 not 8
+    if(!is_not_excluded(route[route.size() - 1], route, not_together_clauses, one_existing_clauses)) {
+        return false;
+    }
+
+    if (route.size() == count_of_nodes) {
         return true;
     } else {
         return false;
@@ -65,11 +71,12 @@ bool route_satisfied(std::vector<int> &route,
 }
 
 bool sat_solver(std::vector<int> &vertexes,
-                const std::unordered_map<int, std::pair<std::pair<double, double>, std::pair<double, double>>>& graph,
+                const std::unordered_map<int, std::pair<std::pair<double, double>, std::pair<double, double>>> &graph,
                 std::vector<int> &route,
                 const std::vector<std::pair<int, int>> &not_together_clauses,
-                const std::vector<std::vector<int>> &one_existing_clauses) {
-    if (route_satisfied(route, graph, not_together_clauses, one_existing_clauses)) { //TODO: does not work correctly
+                const std::vector<std::vector<int>> &one_existing_clauses,
+                const int& count_of_nodes) {
+    if (route_satisfied(route, graph, not_together_clauses, one_existing_clauses, count_of_nodes)) {
         return true;
     }
     std::vector<int> removed_vertexes;
@@ -79,7 +86,7 @@ bool sat_solver(std::vector<int> &vertexes,
             route.emplace_back(*it);
             removed_vertexes.push_back(*it);
             vertexes.erase(it);
-            if (sat_solver(vertexes, graph, route, not_together_clauses, one_existing_clauses)) {
+            if (sat_solver(vertexes, graph, route, not_together_clauses, one_existing_clauses, count_of_nodes)) {
                 return true;
             } else {
                 route.pop_back();
@@ -96,7 +103,7 @@ bool sat_solver(std::vector<int> &vertexes,
 int main() {
     // Beispieldatensatz von 2d-Punktkoordinaten
 
-    /*
+
     std::vector<std::pair<double, double>> coordinates{
             {2.0, 4.0},
             {0.0, 1.0},
@@ -104,20 +111,20 @@ int main() {
             {2.0, 0.0},
             {4.0, 2.0},
     };
-     */
+     /*
     std::vector<std::pair<double, double>> coordinates{
             {0.0, 0.0},
             {1.0, 0.0},
             {0.0, 1.0},
             {1.0, 1.0},
     };
+    */
     int n = int(coordinates.size());
     std::vector<std::pair<int, int>> not_together_clauses; //they cant be after each other
     std::vector<std::vector<int>> one_existing_clauses; //only one of them can exist in solution
     std::unordered_map<int, std::pair<std::pair<double, double>, std::pair<double, double>>> graph;
     graph.reserve(n * (n - 1)); //maximum count of vertexes (because every vertex is stored for every direction)
 
-    //TODO: validate exclusions
     //1. clause that every edge can only used once
     //2. creates vector with all possible edges
     int key = 0;
@@ -203,13 +210,13 @@ int main() {
     }
 
     std::vector<int> result, vertexes;
-    for (auto & i : graph) {
+    for (auto &i: graph) {
         vertexes.push_back(i.first);
     }
-        if (sat_solver(vertexes, graph, result, not_together_clauses, one_existing_clauses)) {
+    if (sat_solver(vertexes, graph, result, not_together_clauses, one_existing_clauses, int(coordinates.size()))) {
         std::cout << "solution" << std::endl;
-        for (int r : result) {
-            if (graph.find(r) == graph.end()) {
+        for (int r: result) {
+            if (graph.find(r) != graph.end()) {
                 auto it = graph.find(r);
                 std::cout
                         << "(" << it->second.first.first
