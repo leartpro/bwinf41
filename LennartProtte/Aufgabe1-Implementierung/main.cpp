dc6d6305871934543d8362095ad1b5900b55b682
leartpro <lenni.protte@gmx.de>
2023-02-04 23:54:04 +0100

    V52 Aufgabe1

--- LennartProtte/Aufgabe1-Implementierung/main.cpp
+++ LennartProtte/Aufgabe1-Implementierung/main.cpp
@@ -17,7 +17,7 @@ using namespace std;
  */
 bool is_reachable(const int &from,
                   const int &to,
-                  vector<int> &route,
+                  const vector<int> &route,
                   const unordered_map<int, pair<pair<double, double>, pair<double, double> > > &graph
 ) {
     if (route.empty() || graph.find(from)->second.second == graph.find(to)->second.first) {
@@ -35,20 +35,20 @@ bool is_reachable(const int &from,
  * @return
  */
 bool is_not_excluded(const int &current,
-                     vector<int> &route,
+                     const vector<int> &route,
                      const vector<pair<int, int> > &not_together_clauses,
                      const vector<vector<int> > &one_existing_clauses) {
     if (route.empty()) {
         return true;
     }
-    for (const pair<int, int> &not_together_clause : not_together_clauses) {
+    for (const pair<int, int> &not_together_clause: not_together_clauses) {
         if (not_together_clause.first == route.back() && not_together_clause.second == current) {
             return false;
         }
     }
-    for (int &vertex : route) {
+    for (const int &vertex: route) {
         if (vertex != current) {
-            for (const vector<int> &clause : one_existing_clauses) {
+            for (const vector<int> &clause: one_existing_clauses) {
                 if (find(clause.begin(), clause.end(), vertex) != clause.end() &&
                     find(clause.begin(), clause.end(), current) != clause.end()) {
                     return false;
@@ -68,23 +68,23 @@ bool is_not_excluded(const int &current,
  * @param one_existing_clauses
  * @return
  */
-bool is_route_satisfied(vector<int> &route,
+bool is_route_satisfied(const vector<int> &route,
                         const unordered_map<int, pair<pair<double, double>, pair<double, double> > > &graph,
-                        const int& count_of_nodes,
+                        const int &count_of_nodes,
                         const vector<pair<int, int> > &not_together_clauses,
                         const vector<vector<int> > &one_existing_clauses
-                        ) {
+) {
     if (route.empty()) {
         return false;
     }
-    for(int i = 0; i < route.size() - 1; i++) {
+    for (int i = 0; i < route.size() - 1; i++) {
         bool is_not_reachable = !is_reachable(route[i], route[i + 1], route, graph);
         bool is_excluded = !is_not_excluded(route[i], route, not_together_clauses, one_existing_clauses);
         if (is_not_reachable || is_excluded) {
             return false;
         }
     }
-    if(!is_not_excluded(route[route.size() - 1], route, not_together_clauses, one_existing_clauses)) {
+    if (!is_not_excluded(route[route.size() - 1], route, not_together_clauses, one_existing_clauses)) {
         return false;
     }
     if (route.size() == count_of_nodes) {
@@ -94,13 +94,76 @@ bool is_route_satisfied(vector<int> &route,
     }
 }
 
+void remove_excluded(vector<int> &vertexes,
+                     const vector<int> &route,
+                     const vector<pair<int, int> > &not_together_clauses,
+                     const vector<vector<int> > &one_existing_clauses
+) {
+    /*
+vertexes.erase(remove_if(
+                   vertexes.begin(),
+                   vertexes.end(),
+                   [&route, &not_together_clauses](int i) {
+                       return any_of(not_together_clauses.begin(), not_together_clauses.end(),
+                                     [&route, &i](pair<int, int> clause) {
+                                         return route[route.size() - 1] == clause.first && i == clause.second;
+                                     });
+                   }),
+           vertexes.end());
+           */
+    for (int i = 0; i < vertexes.size(); i++) {
+        for (const pair<int, int> &clause: not_together_clauses) {
+            if (route.back()== clause.first && vertexes[i] == clause.second) {
+                vertexes.erase(vertexes.begin() + i);
+            }
+        }
+    }
+    /*
+    for(int i = 0; i < route.size(); i++) {
+        for(const vector<int> &clause : one_existing_clauses) {
+            for(int j = 0; j < clause.size(); j++) {
+                if(i == clause[j]) {
+                    for(int k = 0; k < clause.size(); k++) {
+                        if(j != k) {
+                            const auto it = find(vertexes.begin(), vertexes.end(), clause[k]);
+                            if(it != vertexes.end()) {
+                                vertexes.erase(it);
+                            }
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+    }
+*/
+    vector<int> remove_vertexes;
+    for(int & unused_vertex : vertexes) {
+        for (const pair<int, int> &not_together_clause: not_together_clauses) {
+            if (not_together_clause.first == route.back() && not_together_clause.second == unused_vertex) {
+                remove_vertexes.push_back(unused_vertex);
+            }
+        }
+        for (const int &used_vertex : route) {
+            if (used_vertex != unused_vertex) {
+                for (const vector<int> &one_existing_clause : one_existing_clauses) {
+                    if (find(one_existing_clause.begin(), one_existing_clause.end(), used_vertex) != one_existing_clause.end() &&
+                        find(one_existing_clause.begin(), one_existing_clause.end(), unused_vertex) != one_existing_clause.end()) {
+                        remove_vertexes.push_back(unused_vertex);
+                    }
+                }
+            }
+        }
+    }
+}
+
 bool sat_solver(const vector<int> &vertexes,
                 vector<int> &route,
                 const unordered_map<int, pair<pair<double, double>, pair<double, double> > > &graph,
-                const int& count_of_nodes,
+                const int &count_of_nodes,
                 const vector<pair<int, int> > &not_together_clauses,
                 const vector<vector<int> > &one_existing_clauses
-                ) {
+) {
     if (is_route_satisfied(route, graph, count_of_nodes, not_together_clauses, one_existing_clauses)) {
         return true;
     }
@@ -108,13 +171,14 @@ bool sat_solver(const vector<int> &vertexes,
         if (is_reachable(route[int(route.size()) - 1], vertexes[i], route, graph) &&
             is_not_excluded(vertexes[i], route, not_together_clauses, one_existing_clauses)) {
             cout << "current route (size=" << int(route.size()) << ", i=" << i << "): ";
-            for(const auto& p : route) {
+            for (const auto &p: route) {
                 cout << p << " -> ";
             }
             cout << endl;
             route.emplace_back(vertexes[i]);
             vector<int> p_vertexes(vertexes);
             p_vertexes.erase(p_vertexes.begin() + i);
+            remove_excluded(p_vertexes, route, not_together_clauses, one_existing_clauses);
             //TODO: sort vertexes here by distance
             if (sat_solver(p_vertexes, route, graph, count_of_nodes, not_together_clauses, one_existing_clauses)) {
                 return true;
@@ -133,8 +197,8 @@ bool sat_solver(const vector<int> &vertexes,
  * @return 0, wenn es zu keinem RuntimeError oder keiner RuntimeException gekommen ist
  */
 int main() {
-    string input_dir = "../LennartProtte/Aufgabe1-Implementierung/Eingabedateien";
-    string output_dir = "../LennartProtte/Aufgabe1-Implementierung/Ausgabedateien";
+    string input_dir = "../LennartProtte/Aufgabe1-Implementierung/TestInput";
+    string output_dir = "../LennartProtte/Aufgabe1-Implementierung/TestOutput";
 
     //Durchläuft alle Dateien im Eingabeordner
     for (const auto &entry: filesystem::directory_iterator(input_dir)) {
@@ -204,9 +268,9 @@ int main() {
         }
 
         //create clauses entry for every node, it is only allowed to use two of all edges connected to him
-        for (auto coordinate : coordinates) {
+        for (auto coordinate: coordinates) {
             vector<int> from_node, to_node;
-            for (auto &i : graph) {
+            for (auto &i: graph) {
                 if (i.second.first == coordinate) {
                     from_node.push_back(i.first);
                 } else if (i.second.second == coordinate) {
@@ -229,12 +293,13 @@ int main() {
         }
         std::cout << std::endl;
 
-
+        /*
         std::cout << "not_together_clauses: " << std::endl;
         for (const auto &clause: not_together_clauses) {
             std::cout << clause.first << " " << clause.second;
             std::cout << std::endl;
         }
+         */
         /*
         std::cout << "one_existing_clauses: " << std::endl;
         for (const auto &clause: one_existing_clauses) {
@@ -248,13 +313,13 @@ int main() {
 
         //init vertexes
         vector<int> result, vertexes;
-        for (auto &i : graph) {
+        for (auto &i: graph) {
             vertexes.push_back(i.first);
         }
 
         if (sat_solver(vertexes, result, graph, int(coordinates.size()), not_together_clauses, one_existing_clauses)) {
             cout << "solution" << endl;
-            for (int r : result) {
+            for (int r: result) {
                 if (graph.find(r) != graph.end()) {
                     auto value = graph.find(r);
                     cout
