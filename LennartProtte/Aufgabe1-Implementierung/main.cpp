571e19178750a0a5f8390b2970917152aa48a5f4
leartpro <lenni.protte@gmx.de>
2023-03-01 12:33:22 +0100

    V54 latex doc 1

--- LennartProtte/Aufgabe1-Implementierung/main.cpp
+++ LennartProtte/Aufgabe1-Implementierung/main.cpp
@@ -6,29 +6,6 @@
 
 using namespace std;
 
-
-void generate_adi_graph(const vector<pair<double, double>>& result, const string &file) {
-    cout << "\\begin{figure}[!h]\\centering" << endl;
-    cout << "\\NewAdigraph{"<< file.substr(0, file.size() - 4) << "}{" << endl;
-    for(int i = 0; i < result.size(); i++) {
-        cout << "    " << i << ":" << result[i].first/50 << "," <<  result[i].second/50 << ":" << i << ";" << endl;
-    }
-    cout << "}{" << endl;
-    for(int i = 0; i+1 < result.size(); i++) {
-        cout << i << "," << i+1 /*<< ":" << sqrt(pow((result[i].first - result[i+1].first), 2.0) + (pow((result[i].second - result[i+1].second), 2.0)))*/ << ";" << endl;
-    }
-    cout << "}\n\\" << file.substr(0, file.size() - 4) << "{" << endl;
-    for(int i = 0; i < result.size(); i++) {
-        cout << i << (i == result.size() -1 ? "" : ",");
-    }
-    cout << "::red;" << endl;
-    cout << "}" << endl;
-    cout << "\\caption{Figur: " << file.substr(0, file.size() - 4) << "}\n"
-            "        \\label{fig:" << file.substr(0, file.size() - 4) << "}\n"
-            "    \\end{figure}" << endl;
-    cout << "\\newpage" << endl;
-}
-
 /**
  * Berechnet den Winkel zwischen den Vektoren von from_node nach over_node und over_node nach to_node
  * @param over_node der zweite Knoten
@@ -47,6 +24,35 @@ double cross_angle(const pair<double, double> &from_node,
     return angle;
 }
 
+void generate_adi_graph(const vector<pair<double, double>> &result, const string &file) {
+    int fx = 24, fy = 24;
+    cout << "\\begin{figure}[!h]\\centering" << endl;
+    cout << "\\NewAdigraph{" << file.substr(0, file.size() - 4) << "}{" << endl;
+    for (int i = 0; i < result.size(); i++) {
+        if (i != 0 && i != result.size() - 1) {
+            cout << "    " << i << ":" << result[i].first / fx << "," << result[i].second / fy << ":"
+                 << (((float)((int)(cross_angle(result[i - 1], result[i], result[i + 1]) * 10))) / 10) << "° " << ";" << endl;
+        } else if (i == 0) {
+            cout << "    " << i << ":" << result[i].first / fx << "," << result[i].second / fy << ":start;" << endl;
+        } else {
+            cout << "    " << i << ":" << result[i].first / fx << "," << result[i].second / fy << ":end;" << endl;
+        }
+    }
+    cout << "}{" << endl;
+    for (int i = 0; i + 1 < result.size(); i++) {
+        cout << i << "," << i + 1 << ";" << endl;
+    }
+    cout << "}\n\\" << file.substr(0, file.size() - 4) << "{" << endl;
+    for (int i = 0; i < result.size(); i++) {
+        cout << i << (i == result.size() - 1 ? "" : ",");
+    }
+    cout << "::red;" << endl;
+    cout << "}" << endl;
+    cout << "\\caption{Figur: " << file.substr(0, file.size() - 4) << "}\n\\label{fig:"
+         << file.substr(0, file.size() - 4) << "}\n\\end{figure}" << endl;
+    cout << "\\newpage" << endl;
+}
+
 /**
  * Versucht rekursiv mit backtracking eine möglichst kurze Route durch den Graphen zu finden,
  * welche die Kriterien der Aufgabenstellung erfüllt.
@@ -61,7 +67,7 @@ bool solve(vector<pair<double, double> > &route, vector<pair<double, double> > &
         sort(coordinates.begin(), coordinates.end(),
              [p](const auto &lhs, const auto &rhs) {
                  return sqrt(pow((p.first - lhs.first), 2.0) + (pow((p.second - lhs.second), 2.0)))
-                 < sqrt(pow((p.first - rhs.first), 2.0) + (pow((p.second - rhs.second), 2.0)));
+                        < sqrt(pow((p.first - rhs.first), 2.0) + (pow((p.second - rhs.second), 2.0)));
              });
     }
     //Für jeden Knoten
@@ -71,7 +77,7 @@ bool solve(vector<pair<double, double> > &route, vector<pair<double, double> > &
             continue;
         }
         double angle = -1;
-        if(route.size() >= 2) {
+        if (route.size() >= 2) {
             angle = cross_angle(route[route.size() - 2], route.back(), coordinates[i]);
         }
         if (route.empty() ||
@@ -93,7 +99,7 @@ bool solve(vector<pair<double, double> > &route, vector<pair<double, double> > &
         }
     }
 //Wenn es mit der aktuellen Route keine Lösung geben kann
-return false;
+    return false;
 }
 
 /**
@@ -103,7 +109,7 @@ return false;
  * @return 0, wenn es zu keinem RuntimeError oder keiner RuntimeException gekommen ist
  */
 int main() {
-    string input_dir = "../LennartProtte/Aufgabe1-Implementierung/testInput";
+    string input_dir = "../LennartProtte/Aufgabe1-Implementierung/Eingabedateien";
     string output_dir = "../LennartProtte/Aufgabe1-Implementierung/Ausgabedateien";
 
     //Durchläuft alle Dateien im Eingabeordner
@@ -130,18 +136,18 @@ int main() {
         //TODO: Aufgabenkriterien beachten
         //Berechnet den Schwerpunkt der Punktewolke
         double sum_x, sum_y;
-        for(auto & coordinate : coordinates) {
+        for (auto &coordinate: coordinates) {
             sum_x += coordinate.first;
             sum_y += coordinate.second;
         }
         sum_x /= int(coordinates.size());
         sum_y /= int(coordinates.size());
         //Sortiert absteigend nach der Entfernung zum Schwerpunkt der Punktwolke
-        sort(coordinates.begin(), coordinates.end(),
+       /* sort(coordinates.begin(), coordinates.end(),
              [sum_x, sum_y](const auto &lhs, const auto &rhs) {
                  return sqrt(pow((lhs.first - sum_x), 2.0) + (pow((lhs.second - sum_y), 2.0)))
                         > sqrt(pow((rhs.first - sum_x), 2.0) + (pow((rhs.second - sum_y), 2.0)));
-             });
+             });*/
         vector<pair<double, double> > result;
         //Schreibt das Ergebnis in die entsprechende Ausgabedatei
         if (solve(result, coordinates)) {
